130da050d5b5302d5f93439acebebd9cfecb188f
Jack <57134898@qq.com>
2015-04-30 13:04:06 +0800

    aaa

--- ZDCharts/ZDCharts/Handlers/WFFlow.ashx.cs
+++ ZDCharts/ZDCharts/Handlers/WFFlow.ashx.cs
@@ -18,64 +18,45 @@ namespace ZDCharts.Handlers
                 return new Tools.JsonResponse() { Code = "0", Msg = "操作成功", Data = fList };
             }
         }
-        public Tools.JsonResponse Agree()
+        public Tools.JsonResponse Commit()
         {
-            return DoApprove(COMN.MyVars.Yes);
-        }
-
-        public Tools.JsonResponse Refuse()
-        {
-            return DoApprove(COMN.MyVars.No);
-        }
-
-        private Tools.JsonResponse DoApprove(string result)
-        {
-            string curnodeid = context.Request.Form["curnodeid"];
-            if (curnodeid == null || curnodeid == "")
+            string jsonStr = context.Request.Form["pendingdata"];
+            if (jsonStr == null || jsonStr == "")
             {
-                return new Tools.JsonResponse() { Code = "9000", Msg = "curnodeid不能为空" };
+                return new Tools.JsonResponse() { Code = "9000", Msg = "pendingdata不能为空" };
             }
-
+            Newtonsoft.Json.Linq.JArray jArr = Newtonsoft.Json.Linq.JArray.Parse(jsonStr);
             using (DAL.ContractEntities db = new DAL.ContractEntities())
             {
-                Guid curguid = Guid.Parse(curnodeid);
-                var flow = db.V_Flows.SingleOrDefault(p => p.FID == curguid);
-                if (flow == null)
+                foreach (var item in jArr)
                 {
-                    return new Tools.JsonResponse() { Code = "9001", Msg = "未找到节点" };
-                }
-                else
-                {
+                    Guid curguid = Guid.Parse(item["fid"].ToString());
+                    //查找待审批工作流节点
+                    var flow = db.V_Flows.SingleOrDefault(p => p.FID == curguid);
+                    if (flow == null)
+                        return new Tools.JsonResponse() { Code = "9001", Msg = "未找到节点" };
+                    //查找待审批阶段
                     var temrow = db.WF_TemRows.SingleOrDefault(p => p.TemID == flow.TemID && p.RID == flow.RID);
                     if (temrow == null)
-                    {
                         return new Tools.JsonResponse() { Code = "9001", Msg = "未找审批阶段" };
-                    }
-                    else
-                    {
-                        var f = db.WF_Flows.SingleOrDefault(p => p.FID == flow.FID);
-                        if (f == null)
-                        {
-                            return new Tools.JsonResponse() { Code = "9001", Msg = "未找到待审批数据" };
-                        }
-                        else
-                        {
-                            if (temrow.NextID < 0)
-                            {
-                                f.IsFinished = result;
-                            }
-                            f.CurNode = temrow.NextID;
-                        }
-                    }
+                    //查找待审批数据
+                    var f = db.WF_Flows.SingleOrDefault(p => p.FID == flow.FID);
+                    if (f == null)
+                        return new Tools.JsonResponse() { Code = "9001", Msg = "未找到待审批数据" };
+                    //如果没有下一节点则审批借宿
+                    if (temrow.NextID < 0)
+                        f.IsFinished = "Y";
+                    f.CurNode = temrow.NextID;
                     db.WF_Nodes.Add(new DAL.WF_Nodes()
                     {
                         TemRowID = flow.RID,
-                        Result = result,
+                        FlowID = flow.FID,
+                        Result = item["result"].ToString().ToUpper(),
                         CreatedDate = DateTime.Now
                     });
-                    db.SaveChanges();
-                    return new Tools.JsonResponse() { Code = "0", Msg = "操作成功" };
                 }
+                db.SaveChanges();
+                return new Tools.JsonResponse() { Code = "0", Msg = "操作成功" };
             }
         }
     }
