58cbf2435cf49d67b2eb5ed9592156260c174c95
Jack <57134898@qq.com>
2015-08-18 15:03:29 +0800

    费用审批修改

--- ZDCharts/ZDCharts/Handlers/WFFlow1.ashx.cs
+++ ZDCharts/ZDCharts/Handlers/WFFlow1.ashx.cs
@@ -111,69 +111,59 @@ namespace ZDCharts.Handlers
                 return new Tools.JsonResponse() { Code = "9001", Msg = "没有审批数据" };
             using (DAL.ContractEntities db = new DAL.ContractEntities())
             {
+                int rowid = int.Parse(jArr[0]["fid"].ToString().Substring(1));
+                var wf4 = db.WF_Flow4.SingleOrDefault(p => p.ID == rowid);
+                var flow = db.WF_Flows.SingleOrDefault(p => p.FID == wf4.FlowID);
+                var wf3 = db.WF_Flow3.SingleOrDefault(p => p.FlowID == wf4.FlowID);
+                var temrow = db.WF_TemRows.SingleOrDefault(p => p.TemID == flow.TemID && p.RID == flow.CurNode);
                 var rList = jArr.Where(p => p["result"].ToString() == "Y");
                 if (rList.Count() > 0)
                 {
-                    //审批JSON  fid 需要去掉N
-                    //[
-                    //{"fid":"N1","result":"Y"}
-                    //,
-                    //{"fid":"N2","result":"N"}
-                    //]
-                    //TODO没有全部拒绝审批继续 
-                }
-                else
-                {
-                    //TODO全部拒绝审批结束
-                }
-
-                foreach (var item in jArr)
-                {
-                    string result = item["result"].ToString();
-
-                    Guid flowID = Guid.Parse(item["fid"].ToString());
-                    var flow = db.WF_Flows.SingleOrDefault(p => p.FID == flowID);
-                    if (flow == null)
-                        return new Tools.JsonResponse() { Code = "9001", Msg = "未找到节点" };
-                    var temrow = db.WF_TemRows.SingleOrDefault(p => p.TemID == flow.TemID && p.RID == flow.CurNode);
-                    if (temrow == null)
-                        return new Tools.JsonResponse() { Code = "9002", Msg = "未找审批阶段" };
-
-                    //所有行都未通过点则审批结束
-                    if (result == COMN.MyVars.No)
+                    //如果没有下一节,审批结束
+                    if (temrow.NextID == 0)
                     {
                         flow.IsFinished = "Y";
-                        flow.Result = result;
-                        flow.ApprovalStatus = COMN.MyVars.ApprovalStatus_IsRefused;
+                        flow.Result = "Y";
+                        flow.ApprovalStatus = COMN.MyVars.ApprovalStatus_IsAccpeted;
                     }
                     else
                     {
-                        //如果没有下一节,审批结束
-                        if (temrow.NextID == 0)
-                        {
-                            flow.IsFinished = "Y";
-                            flow.Result = result;
-                            flow.ApprovalStatus = COMN.MyVars.ApprovalStatus_IsAccpeted;
-                        }
-                        else
-                        {
-                            flow.IsFinished = "N";
-                            flow.Result = result;
-                            flow.ApprovalStatus = COMN.MyVars.ApprovalStatus_IsHandling;
-                            flow.Result = COMN.MyVars.Pending;
-                        }
+                        flow.IsFinished = "N";
+                        flow.Result = "Y";
+                        flow.ApprovalStatus = COMN.MyVars.ApprovalStatus_IsHandling;
+                        flow.Result = COMN.MyVars.Pending;
                     }
-                    //需要先天剑到NODE后表再赋值
-
                     db.WF_Nodes.Add(new DAL.WF_Nodes()
                     {
                         TemRowID = flow.CurNode,
                         FlowID = flow.FID,
-                        Result = result,
+                        Result = "Y",
                         CreatedDate = DateTime.Now
                     });
-                    flow.CurNode = temrow.NextID;
+                    //TODO没有全部拒绝审批继续 
                 }
+                else
+                {
+                    //所有行都未通过点则审批结束
+                    flow.IsFinished = "Y";
+                    flow.Result = "N";
+                    flow.ApprovalStatus = COMN.MyVars.ApprovalStatus_IsRefused;
+                    db.WF_Nodes.Add(new DAL.WF_Nodes()
+                    {
+                        TemRowID = flow.CurNode,
+                        FlowID = flow.FID,
+                        Result = "N",
+                        CreatedDate = DateTime.Now
+                    });
+                }
+                flow.CurNode = temrow.NextID;
+                foreach (var item in jArr)
+                {
+                    int _rowid = int.Parse(item["fid"].ToString().Substring(1));
+                    var wf4r = db.WF_Flow4.SingleOrDefault(p => p.ID == _rowid);
+                    wf4r.Result = item["result"].ToString();
+                }
+                wf3.Rmb = db.WF_Flow4.Where(p => p.FlowID == wf4.FlowID).Sum(p => p.Rmb);
                 db.SaveChanges();
                 return new Tools.JsonResponse() { Code = "0", Msg = "操作成功" };
             }
